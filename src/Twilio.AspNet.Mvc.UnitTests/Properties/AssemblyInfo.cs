5c0315c4c729bf8a8f035f8232cea4ddaab12311
David Prothero <david@prothero.com>
2017-06-07 10:14:39 -0700

    Version bump

--- src/Twilio.AspNet.Mvc.UnitTests/Properties/AssemblyInfo.cs
+++ src/Twilio.AspNet.Mvc.UnitTests/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Twilio.AspNet.Mvc.UnitTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit tests for Twilio.AspNet.Mvc. Thanks https://github.com/MichaelSL!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Twilio.AspNet.Mvc.UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Twilio")]
+[assembly: AssemblyProduct("Twilio.AspNet")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.2.0")]
+[assembly: AssemblyFileVersion("5.0.2.0")]
