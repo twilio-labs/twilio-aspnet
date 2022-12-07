7d6755bc57e595748e180984b450321721e5f37a
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-12-06 19:53:44 -0500

    use XDocument directly for TwiMLResult.cs

--- src/Twilio.AspNet.Core/TwiMLResult.cs
+++ src/Twilio.AspNet.Core/TwiMLResult.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +18,7 @@ namespace Twilio.AspNet.Core
         public TwiMLResult(TwiML.TwiML twiml) : this(twiml, SaveOptions.None)
         {
         }
-        
+
         /// <param name="twiml">The TwiML to respond with</param>
         /// <param name="formattingOptions">Specifies how to format TwiML</param>
         public TwiMLResult(TwiML.TwiML twiml, SaveOptions formattingOptions)
@@ -29,20 +30,25 @@ namespace Twilio.AspNet.Core
         public async Task ExecuteResultAsync(ActionContext actionContext)
         {
             var response = actionContext.HttpContext.Response;
-            await WriteTwiMLToResponse(response);
+            await WriteTwiMLToResponse(response, actionContext.HttpContext.RequestAborted);
         }
-        
-        private async Task WriteTwiMLToResponse(HttpResponse response)
+
+        private async Task WriteTwiMLToResponse(HttpResponse response, CancellationToken cancellationToken)
         {
             response.ContentType = "application/xml";
             if (twiml == null)
             {
-                await response.WriteAsync("<Response></Response>");
+                await response.WriteAsync("<Response></Response>", cancellationToken);
                 return;
             }
 
-            var data = twiml.ToString(formattingOptions);
-            await response.WriteAsync(data);
+            var doc = twiml.ToXDocument();
+
+#if NET5_0_OR_GREATER
+            await doc.SaveAsync(response.Body, formattingOptions, cancellationToken);
+#else
+            await response.WriteAsync(doc.ToString(formattingOptions), cancellationToken);
+#endif
         }
     }
 }
